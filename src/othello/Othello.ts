210f6002f6ffc06a24001f7c0ff53434bfff5b93
MamadTvl <mtavallai80@gmail.com>
2023-01-09 02:08:04 +0330

    add othello logic and state manager

---
+++ src/othello/Othello.ts
@@ -0,0 +1,126 @@
+import { Context, Player } from '../store';
+
+const moveDirections: [number, number][] = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+];
+
+const isValidBlock = (row: number, column: number) => {
+    return 0 <= row && row < 8 && 0 <= column && column < 8;
+};
+
+export const getPlayerLegalMoves = (
+    player: Player,
+    board: Context['board'],
+): [number, number][] => {
+    const moves: [number, number][] = [];
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            if (isPlayerLegalMove(player, [i, j], board)) {
+                moves.push([i, j]);
+            }
+        }
+    }
+    return moves;
+};
+
+export const hasPlayerLegalMove = (
+    player: Player,
+    board: Context['board'],
+): boolean => {
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            if (isPlayerLegalMove(player, [i, j], board)) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
+
+export const isPlayerLegalMove = (
+    player: Player,
+    move: [number, number],
+    board: Context['board'],
+): boolean => {
+    if (isValidBlock(move[0], move[1]) && !board[move[0]][move[1]]) {
+        for (const direction of moveDirections) {
+            if (canDiscFlip(player, move, direction, board)) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
+
+const canDiscFlip = (
+    player: Player,
+    move: [number, number],
+    direction: [number, number],
+    board: Context['board'],
+): boolean => {
+    let i = 1;
+    while (true) {
+        const row = move[0] + direction[0] * i;
+        const column = move[1] + direction[1] * i;
+        if (!isValidBlock(row, column) || !board[row][column]) {
+            return false;
+        } else if (board[row][column] !== player.color) {
+            break;
+        } else {
+            i++;
+        }
+    }
+    return i > 1;
+};
+
+export const flipDiscs = (
+    player: Player,
+    move: [number, number],
+    board: Context['board'],
+): Context['board'] => {
+    const updatedBoard = [...board];
+    for (const direction of moveDirections) {
+        if (canDiscFlip(player, move, direction, updatedBoard)) {
+            let i = 0;
+            while (true) {
+                const row = move[0] + direction[0] * i;
+                const column = move[1] + direction[1] * i;
+                if (updatedBoard[row][column] === player.color) {
+                    break;
+                } else {
+                    updatedBoard[row][column] = player.color;
+                }
+            }
+        }
+    }
+    return updatedBoard;
+};
+
+export const getPlayerDiscsCount = (player: Player, board: Context['board']): number => {
+    let count = 0;
+    for (const rowBoard of board) {
+        for (const item of rowBoard) {
+            if (item === player.color) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+export const isTheGameOver = (
+    players: Player[],
+    board: Context['board'],
+): boolean => {
+    return (
+        !hasPlayerLegalMove(players[0], board) &&
+        !hasPlayerLegalMove(players[1], board)
+    );
+};
---
+++ src/store/index.ts
@@ -0,0 +1,160 @@
+import {
+    getPlayerLegalMoves,
+    getPlayerDiscsCount,
+    hasPlayerLegalMove,
+} from './../othello/Othello';
+import create from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
+import { flipDiscs } from '../othello/Othello';
+export type Color = 'white' | 'black';
+
+export interface Player {
+    name: string;
+    color: Color;
+    isRobot: boolean;
+    discs: number;
+    legalMoves: [number, number][];
+}
+
+export interface Disc {
+    color: Color;
+}
+
+export interface Context {
+    board: (Color | null)[][];
+    gameOvered: boolean;
+    turn: number;
+    players: Player[];
+    mode: 'robot' | 'friendly';
+    started: boolean;
+}
+
+export interface Store extends Context {
+    putDisc: (index: number, move: [number, number]) => void;
+    updatePlayer: (index: number, details: Partial<Player>) => void;
+    gameOver: () => void;
+    restartGame: () => void;
+    startGame: () => void;
+    changeMode: (mode: Context['mode']) => void;
+    // todo: start the game
+    // todo: change mode of the game
+}
+
+const getInitialBoard = (): (Color | null)[][] => {
+    const board = new Array(8);
+    for (let i = 0; i < board.length; i++) {
+        board[i] = new Array(8).fill(null);
+    }
+    board[3][3] = 'white';
+    board[4][4] = 'white';
+    board[4][3] = 'black';
+    board[3][4] = 'black';
+
+    return board;
+};
+
+const initialStates: Context = {
+    board: getInitialBoard(),
+    gameOvered: false,
+    turn: 0,
+    players: [
+        {
+            name: '',
+            color: 'black',
+            discs: 32,
+            isRobot: false,
+            legalMoves: [],
+        },
+        {
+            name: '',
+            color: 'white',
+            discs: 32,
+            isRobot: false,
+            legalMoves: [],
+        },
+    ],
+    mode: 'friendly',
+    started: false,
+};
+
+export const useOthelloStore = create<Store>()(
+    devtools(
+        persist(
+            (set) => ({
+                ...initialStates,
+                restartGame: () => set(() => initialStates),
+                putDisc: (index, move) => {
+                    set((state) => {
+                        const otherPlayerIndex = index === 0 ? 1 : 0;
+                        let updatedBoard = [...state.board];
+                        const thisPlayer = state.players[index];
+                        const otherPlayer = state.players[otherPlayerIndex];
+                        let turn = state.turn;
+                        const players = [...state.players];
+                        updatedBoard[move[0]][move[1]] = thisPlayer.color;
+                        updatedBoard = flipDiscs(
+                            thisPlayer,
+                            move,
+                            updatedBoard,
+                        );
+                        players[index].legalMoves = getPlayerLegalMoves(
+                            thisPlayer,
+                            updatedBoard,
+                        );
+                        players[otherPlayerIndex].legalMoves =
+                            getPlayerLegalMoves(otherPlayer, updatedBoard);
+                        players[index].discs = getPlayerDiscsCount(
+                            thisPlayer,
+                            updatedBoard,
+                        );
+                        players[otherPlayerIndex].discs = getPlayerDiscsCount(
+                            otherPlayer,
+                            updatedBoard,
+                        );
+                        if (
+                            hasPlayerLegalMove(
+                                players[otherPlayerIndex],
+                                updatedBoard,
+                            )
+                        ) {
+                            turn = otherPlayerIndex;
+                        }
+
+                        return {
+                            board: updatedBoard,
+                            turn,
+                            players,
+                        };
+                    });
+                },
+                updatePlayer: (index, details) =>
+                    set((state) => ({
+                        players: state.players.map((player, i) => ({
+                            ...player,
+                            ...(i === index ? details : {}),
+                        })),
+                    })),
+                gameOver: () => {},
+                startGame: () => {
+                    set((state) => {
+                        return {
+                            players: state.players.map((player) => ({
+                                ...player,
+                                legalMoves: getPlayerLegalMoves(
+                                    player,
+                                    state.board,
+                                ),
+                                discs: getPlayerDiscsCount(player, state.board),
+                            })),
+                            started: true,
+                        };
+                    });
+                },
+                changeMode: (mode) => {},
+            }),
+            {
+                name: 'othello-storage',
+            },
+        ),
+    ),
+);
