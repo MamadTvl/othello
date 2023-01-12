3d12491de8830b2f8b0b6a1d4090a4b7a61c1717
MamadTvl <mtavallai80@gmail.com>
2023-01-12 21:54:00 +0330

    add icon and stuff

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,25 @@ import './App.css';
 import { useOthelloStore } from './store';
 import Board from './components/game/Board';
 import Home from './components/home/Home';
-
 function App() {
     const isGameStarted = useOthelloStore((store) => store.started);
 
-    return <div className='App'>{isGameStarted ? <Board /> : <Home />}</div>;
+    return (
+        <div className='App'>
+            {isGameStarted ? <Board /> : <Home />}
+            <a
+                id={'source_code'}
+                href='https://github.com/mamadtvl/othello'
+                target={'_blank'}>
+                <img
+                    width={50}
+                    height={50}
+                    src={'/github.svg'}
+                    alt={'github'}
+                />
+            </a>
+        </div>
+    );
 }
 
 export default App;
